6664bae1b2fa03f5412e71ac30738712e33bd679
tiwarimanvi <manvitiwari029@gmail.com>
2024-01-26 00:06:04 +0530

    Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
    A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.

---
+++ que1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size();
+        int n = text2.size();
+
+        int dp[m + 1][n + 1];
+
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 0; j <= n; ++j) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
+
+int main() {
+    Solution solution;
+    string text1 = "abcde";
+    string text2 = "ace";
+
+    int result = solution.longestCommonSubsequence(text1, text2);
+
+    cout << "Length of Longest Common Subsequence: " << result << endl;
+
+    return 0;
+}
